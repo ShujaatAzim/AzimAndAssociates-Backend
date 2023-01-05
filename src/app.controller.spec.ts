291f8f0a28f4139cdcefdf56d4356f653677fa12
Shujaat Azim <shujaat.azim@gmail.com>
2023-01-04 23:49:54 -1000

    Edited tests.

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
+      controllers: [AppController, AuthController],
+      providers: [AppService, AuthService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
