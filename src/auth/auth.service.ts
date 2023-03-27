454da06cf95ff3786f327b9373d6ffab96d7b8ac
Shujaat Azim <shujaat.azim@gmail.com>
2023-03-27 12:35:04 -0400

    Built out prisma logic, connected it to others.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
 
   signIn() {
-    return { 
-      message: "I have successfully signed in!" 
+    return {
+      message: 'I have successfully signed in!',
     };
-  };
+  }
 
   signUp() {
-    return { 
-      message: "I have successfully signed up!" 
+    return {
+      message: 'I have successfully signed up!',
     };
-  };
+  }
 
   logout() {
-    return { 
-      message: "Session terminated." 
+    return {
+      message: 'Session terminated.',
     };
-  };
+  }
 }
