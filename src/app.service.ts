f784b9263d45475e648b8567d922dfeba3b7f66c
Shujaat Azim <shujaat.azim@gmail.com>
2023-05-11 22:14:06 -1000

    Removed unneeded comment slashes, made connection message more explicit.

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  //
   successfulConnection() {
-    return { success: true };
+    return { connection_success: true };
   }
 }
