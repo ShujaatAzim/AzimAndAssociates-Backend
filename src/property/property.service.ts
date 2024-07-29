b1d10335ca753aac09e9449e782c5ab0041c83dd
Shujaat Azim <shujaat.azim@gmail.com>
2024-07-28 23:00:59 -0500

    feat: Fix prettier

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,10 +1,10 @@
-import { Get, Delete, Injectable } from '@nestjs/common';
+import { Get, Delete, Injectable } from '@nestjs/common'
 
 interface Property {
-  id: number;
-  property: string;
-  address: string;
-  leaseURL: string;
+  id: number
+  property: string
+  address: string
+  leaseURL: string
 }
 
 @Injectable()
@@ -15,16 +15,16 @@ export class PropertyService {
       { id: 1, property: 'property 1', address: '', leaseURL: '' },
       { id: 2, property: 'property 2', address: '', leaseURL: '' },
       { id: 3, property: 'property 3', address: '', leaseURL: '' },
-    ];
+    ]
   }
 
   @Get()
   getPropertyByID(id: number) {
-    return { propertyID: id };
+    return { propertyID: id }
   }
 
   @Delete()
   deletePropertyByID(id: number) {
-    return { propertyID: id, status: 'deleted' };
+    return { propertyID: id, status: 'deleted' }
   }
 }
